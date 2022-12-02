edb9c3ca3e75129e4802517a6747d117cbcd1f40
Sherif <muhammedsherif40@yahoo.com>
2022-12-02 09:48:15 -0800

    modules

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+def add(a, b):
+    """My addition function
+    Args:
+        a: first interger
+        b: second integer
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
